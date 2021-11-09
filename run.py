1f509e83156261388152d01c5b2c277ca9274164
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2021-11-09 21:49:11 +0100

    Add build board function to Board class

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-
+from pprint import pprint
 
 
 class Board:
@@ -14,17 +14,29 @@ class Board:
         self.num_of_ships = num_of_ships
         self.columns = columns
     
+    def build_board(self):
+        """
+        Builds a blank board.
+        """
+        board = []
+        for row in range(self.rows):
+            board.append([])
+            for _ in range(self.columns):
+                board[row].append("~")
+        return board
 
 class Ship:
     """
     Creates the ship class for later sub class of ships.
     """
-    def __init__(self, length, start_position, end_position, damaged_tiles):
+    def __init__(self, length, start_position, direction, damaged_tiles):
         self.length = length
         self.start_position = start_position
-        self.end_position = end_position
+        self.direction = direction
         self.damaged_tiles = damaged_tiles
 
 
 
 # Build boards
+player_board = Board(10, 10, "Player", 5)
+pprint(player_board.build_board())
