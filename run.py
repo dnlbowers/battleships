e48d3de9847deee78c1fce6f5a3566a4d7b9d658
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2021-11-10 12:45:18 +0100

    Add code for dictionary of coordiantes and comment out for later

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 from pprint import pprint
 
+# This will generate the boards co-ordinates
+#coordinates = [(row, column) for row in range(self.rows) for column in range(self.columns)]
+
+#creates a dictionary of coordinates, will use later to place ships and check for hits
+# {(row,column):"~" for row in range(self.rows) for column in range(self.columns)
+#             if row != 0 and column != 0}
 
 class Board:
     """
@@ -13,10 +17,11 @@ class Board:
         self.name = name
         self.num_of_ships = num_of_ships
         self.columns = columns
-    
+
+
     def build_board(self):
         """
-        Builds a blank board.
+        Builds a blank board with coordinates as a key.
         """
         board = []
         for row in range(self.rows):
