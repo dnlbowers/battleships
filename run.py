6b98e2912d3feb0b36770877bea624998d2c6705
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2021-11-10 23:50:33 +0100

    make several notes from OOP videos which may be of use, abbreviate row and col into singlr param and amend code to suit this

--- run.py
+++ run.py
@@ -7,29 +7,32 @@ from pprint import pprint
 #creates a dictionary of coordinates, will use later to place ships and check for hits
 # {(row,column):"~" for row in range(self.rows) for column in range(self.columns)
 #             if row != 0 and column != 0}
-
+# for the player guess consider a class method to split the string https://youtu.be/rq8cL2XMM5M
 class Board:
     """
     Creates Boards for both the user and the computer.
     """
-    def __init__(self, rows, columns, name, num_of_ships):
-        self.rows = rows
+    def __init__(self, size, name, num_of_ships):
+        self.size = size
         self.name = name
+        #think about making this a class variable(num_of_ships = 5) but may need
+        #to move to ships class
+        #sample - https://www.youtube.com/watch?v=BJ-VvGyQxho
         self.num_of_ships = num_of_ships
-        self.columns = columns
 
-
+    #maybe make this a static method @staticmethod func with no params https://youtu.be/rq8cL2XMM5M
     def build_board(self):
         """
         Builds a blank board with coordinates as a key.
         """
         board = []
-        for row in range(self.rows):
+        for row in range(self.size):
             board.append([])
-            for _ in range(self.columns):
+            for _ in range(self.size):
                 board[row].append("~")
         return board
 
+#this may be a subclass of board to place ships
 class Ship:
     """
     Creates the ship class for later sub class of ships.
@@ -43,5 +46,5 @@ class Ship:
 
 
 # Build boards
-player_board = Board(10, 10, "Player", 5)
+player_board = Board(10, "Player", 5)
 pprint(player_board.build_board())
