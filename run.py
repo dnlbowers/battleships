0a2f39096c664cfbb2dc254deea365bd5b7ada68
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2021-11-12 23:30:43 +0100

    Make board a subclass of player and create ships class

--- run.py
+++ run.py
@@ -5,11 +5,15 @@ class Player:
     """
     Creates a player object
     """
-    num_of_ships = 5
-    board_size = 10
-
     def __init__(self, name):
         self.name = name
+
+
+class Board(Player):
+    """"Build the boards"""
+    board_size = 10
+    def __init__(self, name):
+        super().__init__(name)
         self.board = self.build_board()
 
     def build_board(self):
@@ -22,7 +26,7 @@ class Player:
             for _ in range(self.board_size):
                 board[row].append("~")
         return board
-    
+
     def print_board(self):
         """
         Prints the board.
@@ -32,16 +36,47 @@ class Player:
             print(" ".join(row))
 
 
+class Ships(Board, Player):
+    """
+    Creates a naval fleet to be placed on the board by the player.
+    Give method for auto deployment of ships on the board.
+    Auto deployment is optional for the player but mandatory for the computer.
+    """
+    naval_fleet = {
+        "Carrier": 5,
+        "Battleship": 4,
+        "Cruiser": 3,
+        "Submarine": 3,
+        "Destroyer": 2
+    }
+    #How to __init__ ? 
+
+    #Possible method to place ships on the board:
+    # def place_ship(self, ship, start_row, start_col, end_row, end_col):
+    #     """
+    #     Places a ship on the board.
+    #     """
+    #     if start_row == end_row:
+    #         for col in range(start_col, end_col + 1):
+    #             self.board[start_row][col] = ship
+    #     elif start_col == end_col:
+    #         for row in range(start_row, end_row + 1):
+    #             self.board[row][start_col] = ship
+    #     else:
+    #         raise ValueError("Invalid ship placement.")
+
 player_name = input("What is your name? ")
-player = Player(player_name)
-computer = Player("Computer")
-
-# pprint(player.__dict__)
-# pprint(computer.__dict__)
-
-# print(player.name + ", place your ships!")
-
-player.print_board()
-computer.print_board()
+player = Board(player_name)
+computer = Board("Computer")
 
 
+pprint(player.__dict__)
+pprint(computer.__dict__)
+
+print(player.name + ", place your ships!")
+
+Board.print_board(player)
+Board.print_board(computer)
+
+print(issubclass(Ships, Player))
+print(isinstance(player, Player))
